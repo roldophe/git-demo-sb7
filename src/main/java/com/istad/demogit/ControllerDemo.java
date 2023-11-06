4d6f346e3ddcd36f1d0e1db6fcee2da3da397f41
roldophe <138679281+roldophe@users.noreply.github.com>
2023-11-06 19:52:09 +0700

    Requestbody dynamic Keyword

--- src/main/java/com/istad/demogit/ControllerDemo.java
+++ src/main/java/com/istad/demogit/ControllerDemo.java
@@ -1,6 +1,7 @@
 package com.istad.demogit;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo")
 public class ControllerDemo {
     @GetMapping
-    public String getFirstDemo(){
-        return "Hello, world";
+    public String getFirstDemo(@RequestBody String keyWord){
+        return keyWord;
     }
 }

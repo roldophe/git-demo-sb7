739b9621ca19624b3701f51a29da1235cc6a91c5
roldophe <138679281+roldophe@users.noreply.github.com>
2023-11-06 18:57:34 +0700

    My first initialize commit

---
+++ src/test/java/com/istad/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istad.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

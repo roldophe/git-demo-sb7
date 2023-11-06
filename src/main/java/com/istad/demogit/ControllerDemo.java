c09ccd67f4823ff641e2dffcad26949956e6fe34
roldophe <138679281+roldophe@users.noreply.github.com>
2023-11-06 19:49:08 +0700

    Define Controller

---
+++ src/main/java/com/istad/demogit/ControllerDemo.java
@@ -0,0 +1,14 @@
+package com.istad.demogit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class ControllerDemo {
+    @GetMapping
+    public String getFirstDemo(){
+        return "Hello, world";
+    }
+}
